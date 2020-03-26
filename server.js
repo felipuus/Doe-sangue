617accfac61f02c75617a56c581c591fe6ff8a29
felipuu <luiz.felipe.felipe63@gmail.com>
2020-03-26 13:54:57 -0300

    servidor finalizado

--- server.js
+++ server.js
@@ -5,15 +5,19 @@ const server = express()
 // configurar o servidor para aprensentar arquivos extras
 server.use(express.static('public'))
 
+//habilitar body do formulario
+server.use(express.urlencoded({ extended:true }))
+
 // configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache:true, // booblean ou booleano aceita 2 vaores, verdadeiro ou falso
 })
 
 // lista de doadores: Vetor ou Arry
 
-const dpnors = [
+const donors = [
     {
         name:"Diego Fernandes",
         blood:"AB+"
@@ -36,7 +40,23 @@ const dpnors = [
 
 // configurar a apresentação da página
 server.get("/", function(req,res){
-    return res.render("index.html", { donors })})
+    return res.render("index.html", { donors })
+})
+
+server.post("/", function(req,res) {
+    //pegar dados do formulario 
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    // coloco valores dentro do array
+    donors.push({
+        name:name,
+        blood:blood,
+    })
+
+    return res.redirect("/")
+})
 
 // ligar o servidor e permitir o acesso na porta 4000
 server.listen(4000, function() {
